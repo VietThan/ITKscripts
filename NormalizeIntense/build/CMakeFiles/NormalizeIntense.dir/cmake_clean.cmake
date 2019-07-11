d7b2721c6b14c00d313f772ee5b69ff00ca2851b
VietThanAccre <thanhoangviet@gmail.com>
2019-07-11 11:50:14 -0500

    Making normalize intense script

---
+++ NormalizeIntense/build/CMakeFiles/NormalizeIntense.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NormalizeIntense.dir/NormalizeIntense.cpp.o"
+  "NormalizeIntense.pdb"
+  "NormalizeIntense"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NormalizeIntense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
