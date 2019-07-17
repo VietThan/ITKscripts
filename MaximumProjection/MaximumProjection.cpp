ab8e11e9a038f71a1def6cf4255731947fb27b9d
VietThanAccre <thanhoangviet@gmail.com>
2019-07-17 13:09:17 -0500

    Maximum Projection working

--- MaximumProjection/MaximumProjection.cpp
+++ MaximumProjection/MaximumProjection.cpp
@@ -27,7 +27,7 @@ std::string makeOutputFileName (const std::string &filename, const std::string &
 
 
 
-// 5 arguments:
+// 4 arguments:
 // 1 - filename
 // 2 - type
 // 3 - direction
@@ -44,43 +44,35 @@ int main(int argc, char * argv []){
 
 	// setting up arguments
 	std::string filename, type;
-	int x, y, step;
+	int direction;
 	
 	// constexpr, computation at compile time
-	constexpr unsigned int Dimension = 2;
+	constexpr unsigned int Dimension = 3;
 	constexpr float intensityMinimum = 0.0;
 	constexpr float intensityMaximum = 255.0;
 	
-	if (argc == 6){
+	if (argc == 4){
 		std::cout << "Accepted input arguments" << std::endl;
 		filename = argv[1];
 		type = argv[2];
-		x = atoi(argv[3]);
-		y = atoi(argv[4]);
-		step = atoi(argv[5]);
+		direction = atoi(argv[3]);
 	} else {
 		std::cout << "Not enough arguments, went with default" << std::endl;
-		filename = "slice000"; 
-		type = ".tif";
-		x = 25;
-		y = 25;
-		step = 10;
+		filename = "volume_250_250_200_rescaled_Hessian_0p8_1p0_250p0_2p0_11p0_10"; 
+		type = ".nii.gz";
+		direction = 0;
 
 	}
-	//timing
 
 	std::string inputFileName = makeInputFileName(filename, type);	// input is assumed in ../data/
 	std::string outputFileName = makeOutputFileName(filename, type);
 	
 
-	std::cout << "x: " << x << "\n";
-	std::cout << "y: " << y << "\n";
-	std::cout << "step: " << step << "\n";
 	std::cout << "filename: " << inputFileName << "\n";
 	
 	
 	// setting up reader type
-	using imagePixelType = float;						// float is ITK acceptable
+	using imagePixelType = unsigned char;						// float is ITK acceptable
 	using ImageType = itk::Image< imagePixelType, Dimension>;		// ImageType is used for both input and output
 	using ReaderType = itk::ImageFileReader< ImageType >;
 	
@@ -107,72 +99,15 @@ int main(int argc, char * argv []){
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - begin);
 	std::cout << duration.count() << " milliseconds for reading in the file and creating constants"<<std::endl;
 
-	// get image and region
-	ImageType::Pointer image = reader->GetOutput();
-	ImageType::RegionType region = image->GetLargestPossibleRegion();
-	ImageType::SizeType size = region.GetSize();
-	int width = size[0];
-	int height = size[1];
-
-	if ((x-step)<0 || (x+step)>=width){std::cout<<"step is out of bound x\n";return EXIT_FAILURE;}
-	if ((y-step)<0 || (y+step)>=height){std::cout<<"step is out of bound y\n";return EXIT_FAILURE;}
-
-	// calculating mean
-	imagePixelType totalSum = 0;
-	imagePixelType min = 4;
-	imagePixelType max = 0;
-	imagePixelType pixelNum = (2*step+1)*(2*step+1);
-	for (int i = (x-step); i <= (x + step); ++i){
-		for (int j = (y-step); j <= (y+step); ++j){
-			ImageType::IndexType index = {{ i , j }};
-			imagePixelType curPix = image->GetPixel(index);
-			if (curPix > max){max = curPix;}
-			if (curPix < min){min = curPix;}
-			totalSum += curPix;
-		}
-	}	
-
-	imagePixelType mean = totalSum/pixelNum;
-
-	std::cout << "sum: " << totalSum << "\n";
-	std::cout << "# of pixels: " << pixelNum << "\n";
-	std::cout << "mean: " << mean << "\n";
-	std::cout << "min: " << min << "\n";
-	std::cout << "max: " << max << "\n";
-
-
-	// calculating std.dev.
-	imagePixelType totalSqSum = 0;
-	for (int i = (x-step); i <= (x + step); ++i){
-		for (int j = (y-step); j <= (y+step); ++j){
-			ImageType::IndexType index = {{ i , j }};
-			imagePixelType curPix = image->GetPixel(index);
-			totalSqSum += std::pow((curPix-mean), 2);
-		}
-	}	
-
-	imagePixelType variance = totalSqSum/(pixelNum-1);
-	imagePixelType stdDev = std::sqrt(variance);
-
-	std::cout << "variance: " << variance << "\n";
-	std::cout << "std.dev.: " << stdDev << "\n";
 	
-	stop = std::chrono::high_resolution_clock::now();
-	duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - begin);
-	std::cout << duration.count() << " milliseconds for calculating mean and standard deviation"<<std::endl;
-
-	// setting the pixel values
-	for (int i = 0; i < width; ++i){
-		for(int j = 0; j < height; ++j){
-			ImageType::IndexType index = {{ i , j }};
-			imagePixelType curPix = image->GetPixel(index);
-			image->SetPixel( index, ((curPix-mean)/stdDev) );
-		}
-	}
-	
+	using ProjectionType = itk::MaximumProjectionImageFilter< ImageType , ImageType >;
+	ProjectionType::Pointer projection = ProjectionType::New();
+	projection->SetInput( reader->GetOutput() );
+	projection->SetProjectionDimension( direction );
+	std::cout << "GetProjectionDimension(): " << projection->GetProjectionDimension() << "\n";
 	
 	// write out image
-	writer->SetInput( image );
+	writer->SetInput( projection->GetOutput() );
 	writer->SetUseCompression(true);
 	
 	try {
@@ -200,9 +135,8 @@ std::string makeInputFileName (const std::string &filename, const std::string &i
 
 std::string makeOutputFileName (const std::string &filename, const std::string &filetype){
 	std::string OutputFileName = "../output/";
-	OutputFileName.append(filename);
-	OutputFileName.append("_").append("Norm");
-	OutputFileName.append(filetype);
+	OutputFileName.append("proj_").append(filename);
+	OutputFileName.append(".png");
 	return OutputFileName;
 }
 
